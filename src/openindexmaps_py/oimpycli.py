ce4508ec327eda3c0bd5145b196f64afa43c8f6c
Stephen Appel <srappel930@gmail.com>
2024-07-20 15:26:48 -0500

    add print to file and black

--- src/openindexmaps_py/oimpycli.py
+++ src/openindexmaps_py/oimpycli.py
@@ -5,10 +5,13 @@ import json
 import geojson
 from openindexmaps_py import mapping, oimpy
 import webbrowser
+from jsonschema import validate, ValidationError
+
 
 @click.group()
 def cli():
     pass
+
 
 # print-json:
 @cli.command()
@@ -17,14 +20,31 @@ def cli():
     nargs=1,
     type=click.File(mode="r"),
 )
-@click.option("--indent", "-i", type=int, default=2, help="Define the JSON indentation level")
-@click.option("--aquery", "-q", nargs=2, type=(str, str), default=("", None), help=r"Query the file by key-value pair. e.g. label=Sheet 1")
-#@click.option("--schema", "-s", type=click.Path(exists=True), help="Path to JSON schema for validation")
-def print_json(file, indent, aquery):
+@click.option(
+    "--indent", "-i", type=int, default=2, help="Define the JSON indentation level"
+)
+@click.option(
+    "--aquery",
+    "-q",
+    nargs=2,
+    type=(str, str),
+    default=("", None),
+    help=r"Query the file by key-value pair. e.g. label=Sheet 1",
+)
+@click.option(
+    "--schema",
+    "-s",
+    type=click.Path(exists=True),
+    help="Path to JSON schema for validation",
+)
+@click.option(
+    "--print-to-file", "-f", type=click.Path(), help="Write output to (geo)json file"
+)
+def print_json(file, indent, aquery, schema, print_to_file):
     """Print (Geo)JSON files"""
     content = json.load(file)
     assert isinstance(content, dict)
-    
+
     # Check if there's a query
     if aquery[0] != "":
         k, v = aquery
@@ -43,8 +63,26 @@ def print_json(file, indent, aquery):
         content_JSON = json.dumps(output_OIM.__geo_interface__, indent=indent)
     else:
         content_JSON = json.dumps(content, indent=indent)
-        
+
+    if schema:
+        with open(schema, "r") as schema_file:
+            schema_data = json.load(schema_file)
+            try:
+                validate(instance=content, schema=schema_data)
+            except ValidationError as e:
+                click.echo(f"Validation error: {e.message}\n")
+                return
+
+    if print_to_file:
+        try:
+            with open(print_to_file, "w") as output_file:
+                output_file.write(content_JSON)
+        except Exception as e:
+            click.echo(e)
+            return
+
     click.echo(content_JSON)
+
 
 # map:
 @cli.command()
@@ -53,15 +91,28 @@ def print_json(file, indent, aquery):
     nargs=1,
     type=click.File(mode="r"),
 )
-def map(file):
+@click.option(
+    "--schema",
+    "-s",
+    type=click.Path(exists=True),
+    help="Path to JSON schema for validation",
+)
+def map(file, schema):
     """Create a quick Folium map and open it in the browser"""
     json_data = json.load(file)
-    mapping.create_map(json.dumps(json_data))
-    webbrowser.open("index.html")
+    if schema:
+        with open(schema, "r") as schema_file:
+            schema_data = json.load(schema_file)
+            try:
+                validate(instance=json_data, schema=schema_data)
+                mapping.create_map(json.dumps(json_data))
+                webbrowser.open("index.html")
+            except ValidationError as e:
+                click.echo(f"Validation error: {e.message}\n")
+
 
 # TODO: diff
 # TODO: merge
 
 if __name__ == "__main__":
     cli()
-   
