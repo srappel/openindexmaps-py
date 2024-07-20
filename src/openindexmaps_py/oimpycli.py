66bc85e7645dd544b78c6df08642255f36f3cd0a
Stephen Appel <srappel930@gmail.com>
2024-07-20 14:46:46 -0500

    working before refactor

--- src/openindexmaps_py/oimpycli.py
+++ src/openindexmaps_py/oimpycli.py
@@ -2,6 +2,7 @@
 
 import click
 import json
+import geojson
 from openindexmaps_py import mapping
 import webbrowser
 
@@ -9,20 +10,36 @@ import webbrowser
 def cli():
     pass
 
-# print:
+# print-json:
 @cli.command()
 @click.argument(
-    "files",
-    nargs=-1,
+    "file",
+    nargs=1,
     type=click.File(mode="r"),
 )
-@click.option("--indent", "-i", type=int, default=4, help="Define the JSON indentation level")
-def print(files, indent):
+@click.option("--indent", "-i", type=int, default=2, help="Define the JSON indentation level")
+@click.option("--aquery", "-q", nargs=2, type=(str, str), default=("", None), help=r"Query the file by key-value pair. e.g. label=Sheet 1")
+def print_json(file, indent, aquery):
     """Print (Geo)JSON files"""
-    for file in files:
-        content = json.load(file)
-        content_JSON = json.dumps(content, indent=indent)
-        click.echo(content_JSON)
+    content = json.load(file)
+    assert isinstance(content, dict)
+    
+    # Check if there's a query
+    if aquery[0] != "":
+        k, v = aquery
+        assert isinstance(k, str)
+        click.echo(f"Query: {k}=={v} ({v.__class__})...\n")
+
+        output_features = []
+        for feature in content.get("features"):
+            feature_properties = feature.get("properties")
+            if str(feature_properties.get(k, "")) == v:
+                output_features.append(feature)
+
+        content.update({"features":output_features})
+
+    content_JSON = json.dumps(content, indent=indent)
+    click.echo(content_JSON)
 
 # map:
 @cli.command()
@@ -36,6 +53,9 @@ def map(file):
     json_data = json.load(file)
     mapping.create_map(json.dumps(json_data))
     webbrowser.open("index.html")
+
+# TODO: diff
+# TODO: merge
 
 if __name__ == "__main__":
     cli()
