e63fc734b73d0d8776a9b8604851f08cb0182f83
Stephen Appel <srappel930@gmail.com>
2024-09-15 20:41:23 -0500

    insert and delete functionality

--- src/openindexmaps_py/oimsqlite.py
+++ src/openindexmaps_py/oimsqlite.py
@@ -1,28 +1,29 @@
 import sqlite3
 
 
-def initialize_database():
-    conn = sqlite3.connect("db/datastore.db")
+def initialize_database(db_path):
+    conn = sqlite3.connect(db_path)
     cursor = conn.cursor()
 
-    # Create tables if they don't already exist
+    # Create openindexmaps table
     cursor.execute(
         """
     CREATE TABLE IF NOT EXISTS openindexmaps (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
         name TEXT NOT NULL,
-        json_data TEXT
+        json_data TEXT,
+        metadata TEXT,
+        defaults TEXT
     )
     """
     )
 
+    # Create sheets table
     cursor.execute(
         """
     CREATE TABLE IF NOT EXISTS sheets (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
         openindexmap_id INTEGER,
-        title TEXT NOT NULL,
-        sheet_number INTEGER,
         json_data TEXT,
         FOREIGN KEY (openindexmap_id) REFERENCES openindexmaps(id)
     )
@@ -31,3 +32,43 @@ def initialize_database():
 
     conn.commit()
     conn.close()
+
+
+def insert_openindexmap(db_path, name, json_data, metadata, defaults):
+    conn = sqlite3.connect(db_path)
+    cursor = conn.cursor()
+
+    cursor.execute(
+        """
+    INSERT INTO openindexmaps (name, json_data, metadata, defaults)
+    VALUES (?, ?, ?, ?)
+    """,
+        (name, json_data, metadata, defaults),
+    )
+
+    conn.commit()
+    conn.close()
+
+
+def delete_openindexmap(db_path, name):
+    conn = sqlite3.connect(db_path)
+    cursor = conn.cursor()
+
+    cursor.execute("DELETE FROM openindexmaps WHERE name = ?", (name,))
+
+    conn.commit()
+    conn.close()
+
+
+if __name__ == "__main__":
+    initialize_database("db/datastore.db")
+    # INSERT INTO openindexmaps (name, json_data, metadata, defaults)
+    insert_openindexmap(
+        "db/datastore.db",
+        "Test OIM",
+        '{"type": "FeatureCollection"}',
+        '{"id": "500"}',
+        '{"publsiher": "Default Publishing House"}',
+    )
+    # DELETE FROM openindexmaps WHERE name = ?
+    delete_openindexmap("db/datastore.db", "Test OIM")
